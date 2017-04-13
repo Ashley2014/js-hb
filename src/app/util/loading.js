fcbc8297cd714bfffedbe3e143e6adb54d77c043
xiaoming <1746534101@qq.com>
2017-04-13 18:39:25 +0800

    init

--- src/app/util/loading.js
+++ src/app/util/loading.js
@@ -3,33 +3,57 @@
  require spin.js
  */
 
-import appendHtml from 'app/common/appendHtml'
-var Spinner=require("spin.js");
 
-var loadingHtmlStr=require('./loading.html')
-var $loadingHtml=$(loadingHtmlStr);
+var target;
 
-
-var target = $loadingHtml.get()[0];
-var spinner = new Spinner().spin(target);
+function initLoading() {
+  var Spinner = require("spin.js");
+  var loadingHtmlStr = require('./loading.html')
 
 
+// var $loadingHtml = $(loadingHtmlStr);
+  var div = document.createElement('div');
+  div.innerHTML = loadingHtmlStr;
+  target = div.childNodes[0];
 
-var show=function(){
-    //console.log('er',$loadingHtml,$("body"))
-    $("body").append($loadingHtml);
-    // appendHtml(document.body,loadingHtmlStr)
+// console.info(div,target,div.firstChild)
+// console.info(target)
+
+// var target = $loadingHtml.get()[0];
+  new Spinner().spin(target);
+
+}
+
+
+
+var show = function () {
+  //console.log('er',$loadingHtml,$("body"))
+  // $("body").append($loadingHtml);
+  // console.log(target)
+  if(!target){
+    initLoading()
+  }
+  document.body.appendChild(target);
+  // appendHtml(document.body,loadingHtmlStr)
 };
 
 
-var hide=function(){
-    $loadingHtml.remove();
+var hide = function () {
+  // console.log(target)
+  // $loadingHtml.remove();
+  if (!target) {
+    return
+  }
+  if (!target.parentNode) {
+    return
+  }
+  target.parentNode.removeChild(target)
 };
 
 
-var loading={
-    show,
-    hide,
+var loading = {
+  show,
+  hide,
 };
 
 export default loading
